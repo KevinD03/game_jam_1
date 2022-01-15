b4661d9f460f1dd03388611a6741f6c10c8d4fa2
XuyanDing <kevind99410@gmail.com>
2022-01-15 11:31:28 -0500

    works

--- game-jam-1/Assets/Scripts/Player.cs
+++ game-jam-1/Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField]
+    private float _playerSpeed = 15f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
