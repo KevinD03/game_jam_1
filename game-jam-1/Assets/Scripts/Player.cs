f0c58e0a454e4cd947b7b8d2d1fdffb5eb23b26d
XuyanDing <kevind99410@gmail.com>
2022-01-15 11:59:10 -0500

    change

--- game-jam-1/Assets/Scripts/Player.cs
+++ game-jam-1/Assets/Scripts/Player.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private float _playerSpeed = 15f;
+
+    [SerializeField]
+    private GameObject _peopleePrefab;
+
+    [SerializeField]
+    private peopleSpawnMamger _enemySpawnManager;
+
+    [SerializeField]
+    private peopleSpawnMamger _peopleePrefab;
     // Start is called before the first frame update
     void Start()
     {
--- game-jam-1/Assets/peopleSpawnManager.cs
+++ game-jam-1/Assets/peopleSpawnManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pepoleSpwanManager : MonoBehaviour
+public class peopleSpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
