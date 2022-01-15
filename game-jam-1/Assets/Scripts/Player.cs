9d92d70b04cb1042c2b80d95d5b864f70a585463
XuyanDing <kevind99410@gmail.com>
2022-01-15 12:14:14 -0500

    WORK

--- game-jam-1/Assets/Scripts/Player.cs
+++ game-jam-1/Assets/Scripts/Player.cs
@@ -12,19 +12,48 @@ public class Player : MonoBehaviour
     private GameObject _peopleePrefab;
 
     [SerializeField]
-    private peopleSpawnMamger _enemySpawnManager;
+    private peopleSpawnManager _peopleSpawnManager;
 
     [SerializeField]
-    private peopleSpawnMamger _peopleePrefab;
+    private UI_manager _uiManager;
+
+    [SerializeField]
+    private int _score;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(-20, 0, 0);
+        _peopleSpawnManager = GameObject.Find("peopleSpawnManager").GetComponent<peopleSpawnManager>();
+        _uiManager = GameObject.Find("UI_manager").GetComponent<UI_manager>();
+
+        if (_peopleSpawnManager == null)
+        {
+            Debug.LogError("Spawn manager is null");
+        }
+
+        if (_uiManager == null)
+        {
+            Debug.LogError("UI manager is null");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
+    }
+
+    public void addScore(int score)
+    {
+        _score += score;
+        Debug.Log(_score);
+        //_uiManager.updateScore(_score);
     }
 }
