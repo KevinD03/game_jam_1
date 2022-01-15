f1b196ea83ec1f6d27c1c9749809e25ee0d449de
XuyanDing <kevind99410@gmail.com>
2022-01-15 15:11:06 -0500

    1

--- game-jam-1/Assets/Scripts/TrainNode.cs
+++ game-jam-1/Assets/Scripts/TrainNode.cs
@@ -6,25 +6,47 @@ public class TrainNode : MonoBehaviour
 {
     public bool splitUpwards = true;
 
+    private Player _train;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _train = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnCollsionEnter( Collision collision )
+    /*void OnCollsionEnter(Collider other)
     {
-        if ( collision.collider.CompareTag( "Train" ) )
+        if (other.tag == "Train")
         {
             // train is at this lever, will need to let the train/track controller know to make the turn
             Debug.Log("COLLIDE WITH NODE");
-            EventManager.Fire( "NodeHit", this.gameObject );
+            EventManager.Fire("NodeHit", this.gameObject);
         }
+    }*/
+
+    /*private void OnTriggerEnter(Collider2D other)
+    {
+
+        // enemy collide with player
+        if (other.tag == "Train")
+        {
+            // get player script component
+
+            if (_player != null)
+            {
+                Debug.Log("works");
+            }
+        }
+
+        // enemy collide with projectile from player
+    }*/
+
+    public void trythis(){
+        Debug.Log("what");
     }
 }
